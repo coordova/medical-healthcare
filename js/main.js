626d254cfab0bc89019e3be0065cf29d8a2fd283
coordova <grcordova@gmail.com>
2023-10-03 19:23:24 -0400

    Insove Project - Init Config files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// mobile nav
+const mobileNav = document.querySelector('.mnav');
+const closeBtn = document.querySelector('.mnav__close-btn');
+const closeBtnIcon = document.querySelector('.mnav__close-btn-icon');
+
+const navOpenedClass = 'left-0';
+const navClosedClass = '-left-[300px]';
+const arrowLeftClass = 'ri-arrow-left-s-line';
+const arrowRightClass = 'ri-arrow-right-s-line';
+
+closeBtn.addEventListener('click', () => {
+    if (mobileNav.classList.contains(navClosedClass)) {
+        mobileNav.classList.toggle(navOpenedClass);
+        // console.log(mobileNav.classList)
+
+        closeBtnIcon.classList.toggle(arrowLeftClass);
+        closeBtnIcon.classList.toggle(arrowRightClass);
+    }
+})
